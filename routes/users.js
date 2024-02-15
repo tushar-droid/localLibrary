62026993dfb1e9f37121caf5861c1962dc2d5d75
tushar-droid <63186251+tushar-droid@users.noreply.github.com>
2024-02-15 02:53:25 -0800

    Added Nodemon - added nodemon to update the changes on save

--- routes/users.js
+++ routes/users.js
@@ -5,5 +5,7 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
-
+router.get('/cool', (req, res, next) =>{
+  res.send("You're so cool")
+})
 module.exports = router;
