9c31ccc141119e942d1fe64d7eb22eae3669c359
tushar-droid <63186251+tushar-droid@users.noreply.github.com>
2024-02-24 20:24:34 -0800

    added create bookInstance form

--- controllers/bookInstanceController.js
+++ controllers/bookInstanceController.js
@@ -1,5 +1,7 @@
 const BookInstance = require('../models/bookinstance');
 const asyncHandler = require('express-async-handler');
+const {body, validationResult} = require('express-validator')
+const Book =  require('../models/book')
 
 exports.bookinstance_list = asyncHandler(async(req, res, next) =>{
     const allBookInstance = await BookInstance.find().populate("book").exec();
@@ -7,8 +9,6 @@ exports.bookinstance_list = asyncHandler(async(req, res, next) =>{
         title: "Book Instance List",
         bookinstance_list: allBookInstance,
     })
-
-
 })
 
 exports.bookinstance_detail = asyncHandler(async(req, res, next) =>{
@@ -28,12 +28,43 @@ exports.bookinstance_detail = asyncHandler(async(req, res, next) =>{
 })
 
 exports.bookinstance_create_get = asyncHandler(async(req, res, next) =>{
-    res.send('NOT IMPLEMENTED! BOOK INSTANCE CREATE GET');
+    const allBooks = await Book.find({}, 'title').sort({title: 1}).exec()
+    res.render("bookinstance_form", {title: "Create Book Instance", book_list: allBooks})
 })
 
-exports.bookinstance_create_post = asyncHandler(async(req, res, next) =>{
-    res.send(`NOT IMPLEMENTED BOOKINSTANCE CREATE POST`);
-})
+exports.bookinstance_create_post = [
+    body("book", "Book must be Specified").trim().isLength({min:1}).escape(),
+    body("imprint", "Imprint must be Specified").trim().isLength({min:1}).escape(),
+    body("status").escape(),
+    body("due_back", "Invalid Date").optional({values:"falsy"}).isISO8601().toDate(),
+
+    asyncHandler(async(req,res,next) =>{
+        const errors = validationResult(req);
+        const bookInstance = new BookInstance({
+            book: req.body.book,
+            imprint: req.body.imprint,
+            status: req.body.status,
+            due_back: req.body.due_back
+        })
+
+        if(!errors.isEmpty()){
+            const allBooks = await Book.find({}, "title").sort({title:1}).exec();
+            res.render("bookinstance_form", {
+                title: "Create BookInstance",
+                book_list: allBooks,
+                selected_book: bookInstance.book._id,
+                errors: errors.array(),
+                bookinstance: bookInstance,
+            });
+            return            
+        }
+        else{
+            await bookInstance.save();
+            res.redirect(bookInstance.url)
+        }
+    }),
+]
+
 
 exports.bookinstance_delete_get = asyncHandler(async(req, res, next) =>{
     res.send('NOT IMPLEMENTED! BOOKINSTANCE DELETE GET')
--- models/bookinstance.js
+++ models/bookinstance.js
@@ -27,7 +27,9 @@ BookInstanceSchema.virtual("url").get(function(){
 BookInstanceSchema.virtual("due_back_formatted").get(function () {
     return DateTime.fromJSDate(this.due_back).toLocaleString(DateTime.DATE_MED);
   });
-
+BookInstanceSchema.virtual("due_back_yyyy_mm_dd").get(function(){
+    return DateTime.fromJSDate(this.due_back).toISODate()
+})
 
 
 module.exports = mongoose.model("BookInstance", BookInstanceSchema);
